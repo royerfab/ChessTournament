28e964eb96b2afa972f0727c943a1e662f922728
Fabien Royer <fabroyer.info@gmail.com>
2021-10-12 20:56:45 +0200

    Amélioration de la Vue et du Modele du Projet 4

--- modele.py
+++ modele.py
@@ -4,13 +4,23 @@ db = TinyDB('db.json')
 
 class Player:
     id = None
-    def __init__(self, last_name, first_name, date_of_birth, sexe, classement):
+    def __init__(self, _type, last_name, first_name, date_of_birth, sexe, classement):
+        self._type = _type
         self.last_name = last_name
         self.first_name = first_name
         self.date_of_birth = date_of_birth
         self.sexe = sexe
         self.classement = classement
         #indiquer que le chiffre est forcément positif? Mettre if où on réupère la valeur
+
+    @classmethod
+    def getAllplayer(cls):
+        playerQuery = Query()
+        results = db.search(playerQuery._type == 'player')
+        players = []
+        for r in results:
+            players.append(cls.deserialize(r))
+        return players
 
     def serialize(self):
         player = {'last_name': self.last_name, 'first_name': self.first_name, 'date_of_birth': self.date_of_birth, 'sexe': self.sexe, 'classement': self.classement}
@@ -37,17 +47,18 @@ class Player:
         for result in results:
             print(result)
 
-    def update(self, last_name):
-        playerQuery = Query()
-        results = db.search(playerQuery.last_name == last_name)
-        player = Player.deserialize(results[0])
-        db.update(playerQuery.last_name == last_name)
+    def update(self, data):
+        if type(data)==dict{}:
+            playerQuery = Query()
+            db.update(data, playerQuery.last_name == self.last_name)
+            return True
+        else:
+            return False
 
     def delete(self, last_name):
         playerQuery = Query()
-        results = db.search(playerQuery.last_name == last_name)
-        player = Player.deserialize(results[0])
-        db.remove(self.deserialize)
+        db.remove(playerQuery.last_name == self.last_name)
+        return True
 
 class Tournament:
     def __init__(self, name, place, date, rounds, players, time_control, description):
@@ -59,11 +70,20 @@ class Tournament:
         self.time_control = time_control
         self.description = description
 
+    @classmethod
+    def getAlltournament(cls):
+        tournamentQuery = Query()
+        results = db.search(tournamentQuery._type == 'tournament')
+        tournaments = []
+        for r in results:
+            tournaments.append(cls.deserialize(r))
+        return tournaments
 
     def serialize(self):
         tournament = {'name': self.name, 'place': self.place, 'date': self.date, 'rounds': self.rounds, 'players': self.players, 'time_control': self.time_control, 'descirption': self.description}
         return tournament
 
+    @classmethod
     def deserialize(self, tournament):
         name = tournament['name']
         place = tournament['place']
@@ -86,14 +106,16 @@ class Tournament:
         for result in results:
             print(result)
 
-    def update(self, name):
-        tournamentQuery = Query()
-        results = db.search(tournamentQuery.name == name)
-        tournament = Tournament.deserialize(results[0])
-        db.update(tournamentQuery.name == name)
+    def update(self, data):
+        if type(data)==dict{}:
+            tournamentQuery = Query()
+            db.update(data, tournamentQuery.name == self.name)
+            return True
+        else:
+            return False
 
     def delete(self, name):
         tournamentQuery = Query()
-        results = db.search(tournamentQuery.name == name)
-        tournament = Tournament.deserialize(results[0])
-        db.remove(self)
+        db.remove(tournamentQuery.name == self.name)
+        return True
+
--- vue.py
+++ vue.py
@@ -1,4 +1,25 @@
+from modele import Player
+
 class View:
+
+    def menu(self):
+        print("Bienvenue")
+        print("\n")
+        create_player = input("Créer un joueur")
+        create_tournament = input("Créer un tournoi")
+        show_lists = input("Voir les listes")
+        update_ranking = input("Modifier le classement")
+        if create_player:
+            self.prompt_for_player()
+        if create_tournament:
+            self.prompt_for_tournament()
+        if show_lists:
+            self.show_lists()
+        if update_ranking:
+            self.update_ranking()
+
+
+class player_view:
 
     def prompt_for_player(self):
         last_name = input("tapez le nom du joueur : ")
@@ -6,11 +27,16 @@ class View:
         date_of_birth = input("tapez la date de naissance du joueur : ")
         sexe = input("tapez le sexe du joueur : ")
         classement = input("tapez le classement du joueur : ")
+        back = input("Retour")
         if not last_name or first_name or date_of_birth or sexe or classement :
             return None
         if sexe != 'H' or sexe != 'F' :
             return None
+        if back:
+            self.menu()
         return last_name, first_name, date_of_birth, sexe, classement
+
+class tournament_view:
 
     def prompt_for_tournament(self):
         name = input("tapez le nom du tournoi : ")
@@ -20,17 +46,84 @@ class View:
         players = input("tapez le nombre de joueurs : ")
         time_control = input("tapez le temps du tournoi : ")
         description = input("tapez la description du tournoi : ")
-        if not name or place or date or rounds or players or time_control or description :
+        choose_players = input("Choisissez huit joueurs")
+        validate_tournament = input("Valider la création d'un tournoi")
+        back = input("Retour")
+        if not name or place or date or rounds or players or time_control or description or choose_players :
             return None
-        return name, place, date, rounds, players, time_control, description
+        if validate_tournament:
+            self.create_first_turn()
+        if back:
+            self.menu()
+        return name, place, date, rounds, players, time_control, description, choose_players
 
-    def write_result(self):
-        result = input("tapez le nom du vainqueur : ")
-        return result
+    def create_first_turn(self):
+        turn_name = input("Tapez le nom du tour :")
+        validate_turn = input("Générer automatiquement des paires et lancer le premier tour")
+        back = input("Retour")
+        if not turn_name:
+            return None
+        if validate_turn:
+            self.create_other_turn()
+        if back:
+            self.prompt_for_tournament()
 
-    def prompt_for_new_game(self):
-        print("Souhaitez vous refaire un match ?")
-        choice = input("Y/n: ")
-        if choice == "n":
-            return False
-        return True
+    def create_other_turns(self):
+        result_1 = input("Tapez le résultat du joueur 1 :")
+        result_2 = input("Tapez le résultat du joueur 1 :")
+        turn_name = input("Tapez le nom du tour :")
+        validate_turn = input("Générer automatiquement des paires et lancer un nouveau tour")
+        end_tournament = input("Mettre fin au tournoi")
+        if not result_1 or result_2:
+            return None
+        if validate_turn and not turn_name:
+            return None
+        if validate_turn:
+            self.create_other_turn()
+        if end_tournament:
+            self.update_ranking()
+
+    def update_ranking(self):
+        update_ranking = input("Taper le nom du joueur et son nouveau rang :")
+        back = input("Retour au menu")
+        if back:
+            self.menu()
+
+class lists:
+
+    def show_lists(self):
+        alphabetic_actor_list = input("Liste des acteurs par ordre alphabétique")
+        rank_actor_list = input("Liste des acteurs selon le classement")
+        alphabetic_player_list = input("Liste des joueurs par ordre alphabétique")
+        rank_player_list = input("Liste des acteurs par ordre alphabétique")
+        tournaments_list = input("Liste des tournois")
+        turns_list = input("Liste des tours d'un tournoi")
+        matchs_list = input("Liste des matchs d'un tournoi")
+        if alphabetic_player_list:
+            print(players)
+        if tournaments_list:
+            print(tournaments)
+        if turns_list:
+            self.turns_list()
+        if matchs_list:
+            self.matchs_list()
+
+    def turns_list(self):
+        turns_list = input("Tapez le nom du tournoi :")
+        back = input("Retour")
+        if not turns_list:
+            return None
+        if turns_list:
+            print()
+        if back:
+            self.show_lists()
+
+    def matchs_list(self):
+        matchs_list = input("Tapez le nom du tournoi :")
+        back = input("Retour")
+        if not matchs_list:
+            return None
+        if matchs_list:
+            print()
+        if back:
+            self.show_lists()
